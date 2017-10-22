2db6508f84ab784d8e36b0e721b286917b521bf5
Martin Briand <martinbriands@gmail.com>
2017-10-22 06:48:14 -0400

    buttons are cute and useful now

--- Assets/Scripts/BottomBar.cs
+++ Assets/Scripts/BottomBar.cs
@@ -8,6 +8,7 @@ public class BottomBar : MonoBehaviour {
 
 	public GameObject button;
 	public DataLoader dl;
+	public int childcount;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,16 +20,24 @@ public class BottomBar : MonoBehaviour {
 	}
 
 	public void Refresh(string s) {
-
+		
 		for (int i = transform.childCount - 1; i >= 0; i--) {
 			Destroy (transform.GetChild(i).gameObject);
 		}
 
+		int k = 0;
+		foreach (Session ses in DATA.sessions) {
+			if (ses.gameName == dl.game && ses.lookData.dictionary.ContainsKey(s)) {
+				k++;
+			}
+		}
 		foreach (Session ses in DATA.sessions) {
 			if (ses.gameName == dl.game && ses.lookData.dictionary.ContainsKey(s)) {
 				GameObject a = Instantiate (button, transform);
 				a.GetComponentInChildren<Text> ().text = ses.sessionID;
+				a.transform.GetComponent<RectTransform>().sizeDelta = new Vector2 (80,  1235/k);
 			}
 		}
 	}
+		
 }
